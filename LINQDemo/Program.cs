3cc186dea7ff2d1f000cf8029111e1d7770add1b
pbishal <pbishal60@gmail.com>
2021-05-19 23:06:58 +0530

    Retrive all Records whose rating is greater than3 and productid is 1 4 & 9

--- LINQDemo/Program.cs
+++ LINQDemo/Program.cs
@@ -18,19 +18,20 @@ namespace LINQDemo
             {
                 new ProductReview(){ProductId=1, UserId=1, Review="Good", Rating=20, IsLike=true},
                 new ProductReview(){ProductId=3, UserId=1, Review="Good", Rating=1, IsLike=false},
-                new ProductReview(){ProductId=1, UserId=1, Review="Good", Rating=17, IsLike=true},
+                new ProductReview(){ProductId=4, UserId=1, Review="Good", Rating=17, IsLike=true},
                 new ProductReview(){ProductId=1, UserId=1, Review="Average", Rating=17, IsLike=true},
-                new ProductReview(){ProductId=1, UserId=1, Review="Bad", Rating=2, IsLike=false}
+                new ProductReview(){ProductId=9, UserId=1, Review="Bad", Rating=2, IsLike=false}
             };
             //IterateOverProductList(list);
-            RetriveTop3RecordsFromList(list);
+            //RetriveTop3RecordsFromList(list);
+            RetriveredsBasedOnRatingAndProductId(list);
             Console.ReadLine();
         }
-        public static void IterateOverProductList(List<ProductReview>list)
+        public static void IterateOverProductList(List<ProductReview> list)
         {
-            foreach(ProductReview product in list)
+            foreach (ProductReview product in list)
             {
-                Console.WriteLine("ProductId:" + product.ProductId + "\t" + "UserId:" + product.UserId + "\t" + 
+                Console.WriteLine("ProductId:" + product.ProductId + "\t" + "UserId:" + product.UserId + "\t" +
                     "Review:" + product.Review + "\t" + "Rating:" + product.Rating + "\t" + "IsLike" + product.IsLike + "\t");
             }
         }
@@ -49,21 +50,38 @@ namespace LINQDemo
         {
             var result = from product in table.AsEnumerable() select product.Field<string>("ProductName");
 
-            foreach(var res in result)
+            foreach (var res in result)
             {
                 Console.WriteLine("Productname:" + res);
             }
         }
 
         //UC2
-        public static void RetriveTop3RecordsFromList(List<ProductReview>list)
+        public static void RetriveTop3RecordsFromList(List<ProductReview> list)
         {
             var result = from product in list orderby product.Rating descending select product;
             var topThreeRecords = result.Take(3);
-            foreach(ProductReview product in topThreeRecords)
+            foreach (ProductReview product in topThreeRecords)
             {
                 Console.WriteLine("ProductId:" + product.ProductId + "\t" + "UserId:" + product.UserId + "\t" +
                     "Review:" + product.Review + "\t" + "Rating:" + product.Rating + "\t" + "IsLike" + product.IsLike + "\t");
+            }
+        }
+
+        //UC3
+        /// <summary>
+        /// Method to retrive records whose rating is greter than 3
+        /// and the product ID is either 1 or 4 or 9
+        /// </summary>
+        /// <param name="list"></param>
+        public static void RetriveredsBasedOnRatingAndProductId(List<ProductReview>list)
+        {
+            //Method Syntax For LINQ
+            var data = list.Where(r => r.Rating > 3 && (r.ProductId == 1 || r.ProductId == 4 || r.ProductId == 9));
+            foreach(var element in data)
+            {
+                Console.WriteLine("ProductId" + element.ProductId + "\t" + "Rating" + element.Rating);
+                Console.WriteLine("--------------");
             }
         }
     }
